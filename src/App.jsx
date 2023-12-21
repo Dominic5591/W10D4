116149435611ce1a8f8e0b795f40ecaa89dea82b
Dominic <domcashiola@gmail.com>
2023-12-21 16:39:55 -0500

    added stying to links

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import GalleryNavigation from './components/GalleryNavigation';
 import harvardArt from './data/harvardArt';
 console.log(harvardArt);
 const router = createBrowserRouter([
-  { path: "*", element: <h2>Page Not Found</h2> },
-  { path: "/galleries", element: <GalleryNavigation galleries={harvardArt.records} /> },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/", 
+        element: 
+          <>
+            <h2>Harvard Art Museum</h2>
+            <p>
+              Look, but Don&apos;t Touch. Please select a Gallery in the
+              navigation bar.
+            </p>
+          </>,
+      },
+      { 
+        path: "*",
+        element: <h2>Page Not Found</h2>,
+      },
+    ],
+  },
 ]);
 
 function App() {
   return <RouterProvider router={router}/>;
   
 }
+
+function Layout() {
+  return (
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 
 export default App;
--- src/components/GalleryNavigation/GalleryNavigation.jsx
+++ src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -1,8 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import "./GalleryNavigation.css";
+
 const GalleryNavigation = ({ galleries }) => {
+  // Debugging or testing, remove it once verified
   console.log(galleries);
+
   return (
-    <h1>Galleries</h1>
+    <nav>
+      <h1>Galleries</h1>
+      <NavLink  to="/">Home</NavLink>
+
+      {galleries.map(gallery => (
+        <NavLink
+          key={gallery.id}
+          to={`/galleries/${gallery.id}`}
+          // activeClassName="active"
+        >
+          {gallery.name}
+        </NavLink>
+      ))}
+    </nav>
   );
 };
 
-export default GalleryNavigation; 
+export default GalleryNavigation;
